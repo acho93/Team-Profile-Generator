dea31ac6a26f695e2d8d5988565bad75315ae239
Ashley Cho <ashleycho.slp@gmail.com>
2021-12-14 02:39:16 -0800

    employee question prompts

--- index.js
+++ index.js
@@ -40,14 +40,16 @@ const addManager = () => {
             type: 'input',
             name: 'email',
             message: "What is the manager's email?",
-            // validate: emailInput => {
-            //     if (emailInput) {
-            //       return true;
-            //     } else {
-            //       console.log("Please enter a valid email!");
-            //       return false;
-            //     }
-            //   }
+            validate: emailInput => {
+                if (String(emailInput).toLowerCase().match(
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                )) {
+                  return true;
+                } else {
+                  console.log("Please enter a valid email!");
+                  return false;
+                }
+              }
         },
         {
             type: 'input',
@@ -82,11 +84,87 @@ Add Employees to the Team
         {
             type: 'list',
             name: 'role',
-            message: ''
-        }
-
+            message: "What is the employee's role?",
+            choices: ["Engineer", "Intern"]
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's name?",
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log("Please enter the employee's name!");
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's ID?",
+            validate: idInput => {
+                if (isNaN(idInput)) {
+                  console.log("Please enter a numerical ID!");  
+                  return false;
+                } else { 
+                  return true;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email?",
+            validate: emailInput => {
+                if (String(emailInput).toLowerCase().match(
+                  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                )) {
+                  return true;
+                } else {
+                  console.log("Please enter a valid email!");
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is the engineer's GitHub?",
+            when: (input) => input.role === "Engineer",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                  } else {
+                    console.log("Please enter the engineer's GitHub!");
+                    return false;
+                  }
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What school did the intern attend?",
+            when: (input) => input.role === "Intern",
+            validate: schoolInput => {
+                if (schoolInput) {
+                    return true;
+                  } else {
+                    console.log("Please enter the intern's school!");
+                    return false;
+                  }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmEmployee',
+            message: "Would you like to add more employees?",
+            default: false
+        },
     ])
-
+    
+};
 
 addManager()
     .then
